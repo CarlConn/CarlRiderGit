e19c9afcd138a76a780f1710d7fc67f3323c610f
Carl Conn <CarlConn@aaawa.com>
2021-08-30 13:57:00 -0700

    first commit

---
+++ CarlRiderGitLearn/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarlRiderGitLearn
+{
+    public class Class1
+    {
+        public Class1(int valvue)
+        {
+            Valvue = valvue;
+        }
+
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public int Age { get; set; }
+        public int Valvue { get; set; }
+    }
+}
